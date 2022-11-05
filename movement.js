49ef81092c6c5378ec9b54f940358856b094f921
Manuel Montoya <manuelm1209@hotmail.com>
2022-11-05 11:14:12 -0500

    Adding second and third way of showing the animation

--- movement.js
+++ movement.js
@@ -13,18 +13,37 @@ setInterval(() => {
         x: d * 70 + 40,
         y: (height / 2) + Math.sin(d * 0.5 + t) * 180,
     }));
-    // First way with DUPLICATED LOGIC.
-    const circles = svg.selectAll('circle').data(data);
-    circles.enter().append('circle')
+    
+    // // First way with DUPLICATED LOGIC.
+    // const circles = svg.selectAll('circle').data(data);
+    // circles.enter().append('circle')
+    //     .attr('r', 30)
+    //     .attr('cx', (d) => d.x)
+    //     .attr('cy', (d) => d.y);;
+    // circles
+    //     .attr('cx', (d) => d.x)
+    //     .attr('cy', (d) => d.y);
+
+    // // Second way using .merge
+    // //Next is the update selection.
+    // const circles = svg.selectAll('circle').data(data);
+    // // Next is the enter selection.
+    // const circlesEnter = circles.enter().append('circle')
+    //     .attr('r', 30);
+
+    // circles.merge(circlesEnter)
+    //     .attr('cx', (d) => d.x)
+    //     .attr('cy', (d) => d.y);
+
+
+    // Third way using .join
+    const circles = svg.selectAll('circle').data(data).join('circle')
         .attr('r', 30)
-        .attr('cx', (d) => d.x)
-        .attr('cy', (d) => d.y);;
-    circles
         .attr('cx', (d) => d.x)
         .attr('cy', (d) => d.y);
 
     // Increase time.
-    t = t + 0.1;
+    t = t + 0.03;
     
-// TIme in miliseconds.  
-}, 100);
+// TIme in miliseconds (60 frames/second).  
+}, 1000 / 60);
